1b986eb0dd43dead2f161c3a1abefc6cf613b227
Diego Molina <dimolinas@unal.edu.co>
2024-06-03 18:03:56 -0500

    Initial Commit

---
+++ TimetonicBooks/TimetonicBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimetonicBooksApp.swift
+//  TimetonicBooks
+//
+//  Created by Diegoe012 on 3/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TimetonicBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
