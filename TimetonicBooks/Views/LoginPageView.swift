d7b5ca7ef32974724b1f9eefbd2179c2625be71b
Diego Molina <dimolinas@unal.edu.co>
2024-06-05 22:15:44 -0500

    Create Login View

--- TimetonicBooks/Views/LoginPageView.swift
+++ TimetonicBooks/Views/LoginPageView.swift
@@ -14,13 +14,43 @@ struct LoginPageView: View {
     
     var body: some View {
         VStack{
+            
+            Text("Login")
+                .font(.largeTitle)
+                .padding(.bottom, 40)
+            
             TextField("Username", text: $username)
+                .padding()
+                .autocapitalization(.none)
+                .background(Color(.secondarySystemBackground))
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
                 .autocorrectionDisabled(true)
             
             SecureField("Password", text: $password)
+                .padding()
+                .background(Color(.secondarySystemBackground))
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            
+            Button{
+                handleSignIn()
+            } label: {
+                Text("Sign In")
+                    .foregroundColor(.white)
+                    .font(.system(size: 16, weight: .semibold))
+                    .frame(maxWidth: .infinity, minHeight: 50)
+                    .background(.black)
+                    .cornerRadius(15)
+            }
             
         }
+        .padding(20)
     }
+}
+
+func handleSignIn(){
+    print("sign in")
 }
 
 #Preview {
